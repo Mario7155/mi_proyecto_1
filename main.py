bf3a72e168e17b7d4e8b55d8317bda4493c5b63a
Mario Alejandro <mario715ach@gmail.com>
2025-09-07 16:00:19 -0500

    feat: integrar worker + flask dummy para Render

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 
 # 🔹 Configuración
 SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQLN4AQYAIb-etfCO_Fvjn62FoCvb4EIsBm28mE1cPiQuGT9wDwfQsEKUiyX0ZdMQ/pub?output=csv"
-API_KEY = "3bd23448befca374ae3f9b4d0bedf3b21f970645dc90e58d71e479f4c8e8fc04"
+API_KEY = "bb9700866ecd82bbe0028531233949c16ec09590d86deaa456e65b45a8d2aad1"
 API_URL = "https://wasenderapi.com/api/send-message"
 HEADERS = {"Authorization": f"Bearer {API_KEY}", "Content-Type": "application/json"}
 
